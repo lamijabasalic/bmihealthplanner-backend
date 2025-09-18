b6a3aed088307a93ab2d814c97912a9bf39f9382
lamijabasalic <lamijabasalic2205@gmail.com>
2025-09-18 22:58:26 +0100

    feat: Add automatic .env file loading with dotenv-java

--- src/main/java/com/example/simple/HealthPlannerSimpleApplication.java
+++ src/main/java/com/example/simple/HealthPlannerSimpleApplication.java
@@ -1,11 +1,24 @@
 package com.example.simple;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthPlannerSimpleApplication {
   public static void main(String[] args) {
+    // Load .env file
+    Dotenv dotenv = Dotenv.configure()
+        .directory("./")
+        .ignoreIfMalformed()
+        .ignoreIfMissing()
+        .load();
+    
+    // Set system properties from .env file
+    dotenv.entries().forEach(entry -> {
+      System.setProperty(entry.getKey(), entry.getValue());
+    });
+    
     SpringApplication.run(HealthPlannerSimpleApplication.class, args);
   }
 }
